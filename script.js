a351df04de93c2f13015fe6dbb6f86f208d4516a
sedoy113 <sedoy113@yandex.ru>
2025-02-16 14:02:59 +0300

    add image

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
+	effect: "cube",
+	grabCursor: true,
+	cubeEffect: {
+		shadow: true,
+		slideShadows: true,
+		shadowOffset: 20,
+		shadowScale: 0.94,
+	},
+	pagination: {
+		el: ".swiper-pagination",
 	},
 });
